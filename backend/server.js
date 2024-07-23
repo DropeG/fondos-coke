0cb0638a15910d8152460e8f1bac40bf84c2be08
DropeG <pedrogh9@gmail.com>
2024-07-23 19:11:13 -0400

    Table results and style changes

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ const upload = multer({storage: storage});
 
 app.post('/upload', upload.fields([{ name: 'sfFile' }, { name: 'odFile' }]), async (req, res) => {
     try {
-        console.log('Files:', req.files);
-        console.log('Body:', req.body);
+        //console.log('Files:', req.files);
+        //console.log('Body:', req.body);
 
         if (!req.files.sfFile || !req.files.odFile){
             throw new Error('Both files must be uploaded');
@@ -33,7 +33,6 @@ app.post('/upload', upload.fields([{ name: 'sfFile' }, { name: 'odFile' }]), asy
 
         const sfFilePath = `/tmp/${sfFile.originalname}`;
         const odFilePath = `/tmp/${odFile.originalname}`;
-
         //Save files to temporary paths
         await writeFile(sfFilePath, sfFile.buffer);
         await writeFile(odFilePath, odFile.buffer);
@@ -47,8 +46,12 @@ app.post('/upload', upload.fields([{ name: 'sfFile' }, { name: 'odFile' }]), asy
 
         res.status(200).json(JSON.parse(result));
     } catch (error) {
-        console.error(error);
-        res.status(500).json({error: error.message});
+        if (error.message.includes('incorrect password')) {
+            res.status(200).json({passwordRequired: true});
+        } else{
+            console.error(error);
+            res.status(500).json({error: error.message});
+        }
     }
 });
 
--- src/FileUploader.js
+++ src/FileUploader.js
@@ -91,17 +91,48 @@ function FileUploader() {
     };
 
     const renderResultadosTable = (data) => {
+        const rowIns = [];
+        const rowF = [];
+        const rowSF = [];
         const rows = [];
+        let fechaSF = '';
         for (const [key, value] of Object.entries(data)) {
-            for (const [subKey, subValue] of Object.entries(value)) {
-                rows.push(
-                    <tr key={subKey}>
-                        <td>{subKey}</td>
-                        <td>{subValue}</td>
-                    </tr>
-                );
+            console.log(key)
+            if (key === 'formattedDate') {
+                fechaSF = value;
+            }
+            else if (key === 'serviciosFinancieros') {
+                for (const [subKeySF, subValueSF] of Object.entries(value)){
+                    rowIns.push(subKeySF);
+                    rowSF.push(subValueSF);
+                }
+            } else {
+                for (const [subKey, subValue] of Object.entries(value)) {
+                    rowF.push(subValue);
+                }
             }
         }
+
+        const len = rowIns.length
+
+        let [day, month, year] = fechaSF.split('-').map(Number);
+        let fechaDate = new Date(year, month-1, day);
+
+        fechaDate.setDate(fechaDate.getDate()+1);
+        let newDay = String(fechaDate.getDate()).padStart(2, '0');
+        let newMonth = String(fechaDate.getMonth() + 1).padStart(2, '0'); // Sumamos 1 al mes para formatearlo correctamente
+        let fechaSig = `${newDay}-${newMonth}`;
+
+        for (let i = 0; i<len; i++) {
+            rows.push(
+                <tr>
+                    <td>{rowIns[i]}</td>
+                    <td>{rowSF[i]}</td>
+                    <td>{rowF[i]}</td>
+                </tr>
+            )
+        }
+
 
         return (
             <div className='table-container'>
@@ -110,7 +141,8 @@ function FileUploader() {
                     <thead>
                         <tr>
                             <th>INSTRUMENTO</th>
-                            <th>POSICION</th>
+                            <th>POSICION {fechaSig}</th>
+                            <th>POSICION {fechaSF.slice(0,5)}</th>
                         </tr>
                     </thead>
                     <tbody>{rows}</tbody>
@@ -140,9 +172,9 @@ function FileUploader() {
                 <table>
                     <thead>
                         <tr>
-                            <th>Company</th>
-                            <th>Position</th>
-                            <th>Tipo de Venta</th>
+                            <th>INSTRUMENTO</th>
+                            <th>POSICION</th>
+                            <th>TIPO DE VENTA</th>
                         </tr>
                     </thead>
                     <tbody>{rows}</tbody>
@@ -151,66 +183,67 @@ function FileUploader() {
         );
     };
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div className='container'>
-                    <h2 className='sf'>Servicios Financieros:</h2>
-                    <div {...getRootPropsSf()} className='dropzone'>
-                        <input {...getInputPropsSf()} />
-                        <div className='inner'>
-                            {!sfFile && (
-                                <>
-                                    <FontAwesomeIcon icon={faUpload} className='upload-icon'/>
-                                    <p className='text'>
-                                        {isDragActiveSf ? 'Drop the files here ...' : 'Upload file'}
-                                    </p>
-                                </>
-                            )}
-                            {sfFile && <p className='file-name'>{sfFile.name}</p>}
-                            {sfFile && <FontAwesomeIcon icon={faCheckCircle} size='sm' className='check-icon' />}
+        <div className='page-layout'>
+            <div className='section'>
+                <form onSubmit={handleSubmit}>
+                    <div className='container'>
+                        <h2 className='sf'>Servicios Financieros:</h2>
+                        <div {...getRootPropsSf()} className='dropzone'>
+                            <input {...getInputPropsSf()} />
+                            <div className='inner'>
+                                {!sfFile && (
+                                    <>
+                                        <FontAwesomeIcon icon={faUpload} className='upload-icon'/>
+                                        <p className='text'>
+                                            {isDragActiveSf ? 'Drop the files here ...' : 'Upload file'}
+                                        </p>
+                                    </>
+                                )}
+                                {sfFile && <p className='file-name'>{sfFile.name}</p>}
+                                {sfFile && <FontAwesomeIcon icon={faCheckCircle} size='sm' className='check-icon' />}
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className='container'>
-                    <h2 className='sf'>Operaciones Diarias:</h2>
-                    <div {...getRootPropsOd()} className='dropzone'>
-                        <input {...getInputPropsOd()} />
-                        <div className='inner'>
-                            {!odFile && (
-                                <>
-                                    <FontAwesomeIcon icon={faUpload} className='upload-icon'/>
-                                    <p className='text'>
-                                        {isDragActiveOd ? 'Drop the files here ...' : 'Upload file'}
-                                    </p>
-                                </>
-                            )}
-                            {odFile && <p className='file-name'>{odFile.name}</p>}
-                            {odFile && <FontAwesomeIcon icon={faCheckCircle} size='sm' className='check-icon' />}
+                    <div className='container'>
+                        <h2 className='sf'>Operaciones Diarias:</h2>
+                        <div {...getRootPropsOd()} className='dropzone'>
+                            <input {...getInputPropsOd()} />
+                            <div className='inner'>
+                                {!odFile && (
+                                    <>
+                                        <FontAwesomeIcon icon={faUpload} className='upload-icon'/>
+                                        <p className='text'>
+                                            {isDragActiveOd ? 'Drop the files here ...' : 'Upload file'}
+                                        </p>
+                                    </>
+                                )}
+                                {odFile && <p className='file-name'>{odFile.name}</p>}
+                                {odFile && <FontAwesomeIcon icon={faCheckCircle} size='sm' className='check-icon' />}
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <button className='submit' type='submit'>Submit</button>
-            </form>
-            {prompts.length > 0 && (
-                <form className = 'prompt-form' onSubmit={handleUserInputSubmit}>
-                    <h3 className='prompt-title'>Tipo de Venta:</h3>
-                    {prompts.map((prompt, index) => (
-                        <div key={index} className='prompt-container'>
-                            <label className='prompt-label'>{prompt}:</label>
-                            <input className='prompt-input' type="text" name={prompt} onChange={handleUserInputChange} />
-                        </div>
-                    ))}
-                    <button className='submit' style={{marginTop: '20px'}} type="submit">Submit</button>
+                    <button className='submit' type='submit'>Submit</button>
                 </form>
-            )}
-            {tableData && (
-                <div>
-                    {renderResultadosTable(tableData)}
-                    {renderOtherTable(tableData)}
-                </div>
-            )}
+                {prompts.length > 0 && (
+                    <form className = 'prompt-form' onSubmit={handleUserInputSubmit}>
+                        <h3 className='prompt-title'>Tipo de Venta:</h3>
+                        {prompts.map((prompt, index) => (
+                            <div key={index} className='prompt-container'>
+                                <label className='prompt-label'>{prompt}:</label>
+                                <input className='prompt-input' type="text" name={prompt} onChange={handleUserInputChange} />
+                            </div>
+                        ))}
+                        <button className='submit' style={{marginTop: '20px'}} type="submit">Submit</button>
+                    </form>
+                )}
+            </div>
+            <div className='section'>
+                {tableData && renderResultadosTable(tableData)}
+            </div>
+            <div className='section'>
+                {tableData && renderOtherTable(tableData)}
+            </div>
         </div>
     );
 }
